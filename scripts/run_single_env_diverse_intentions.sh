88d85588215a74810e74ecace949a477eb8a5255
Khanh Nguyen <kn5378@wash.cs.princeton.edu>
2023-07-25 23:46:44 -0400

    update

--- scripts/run_single_env_diverse_intentions.sh
+++ scripts/run_single_env_diverse_intentions.sh
@@ -2,11 +2,12 @@
 
 #rm outputs/single_env_diverse_intentions && python src/main.py exp_dir=single_env_diverse_intentions_run2 env=messenger tokenizer=messenger actor_critic=messenger world_model=messenger_dummy common.device=cuda:0 collection.train.config.num_steps=1000 training.world_model.start_after_epochs=5 training.world_model.batch_num_samples=8 training.tokenizer.start_after_epochs=1000 training.world_model.grad_acc_steps=4 training.actor_critic.start_after_epochs=1000 wandb.mode=online agent._target_=agent.MessengerRuleBasedAgent
 
-cd ..
+exp_dir=single_env_diverse_intentions
 
-rm outputs/single_env_diverse_intentions && \
-  python src/main.py \
-    exp_dir=single_env_diverse_intentions \
+if [ -d "$exp_dir" ]; then rm -Rf $exp_dir; fi
+
+python3 src/main.py \
+    exp_dir=${exp_dir} \
     env=messenger \
     tokenizer=messenger \
     actor_critic=messenger \
@@ -18,5 +19,5 @@ rm outputs/single_env_diverse_intentions && \
     training.tokenizer.start_after_epochs=1000 \
     training.world_model.grad_acc_steps=4 \
     training.actor_critic.start_after_epochs=1000 \
-    wandb.mode=online \
+    wandb.mode=disabled \
     agent._target_=agent.MessengerRuleBasedAgent
